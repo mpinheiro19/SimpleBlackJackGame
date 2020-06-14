f378e9521cd7f414b61c71ec46821d94002934b6
Marco Antonio Silva Pinheiro <mpinheiro19@gmail.com>
2020-06-13 22:53:46 -0300

    Can hit cards and display some stuff

--- blackjack.py
+++ blackjack.py
@@ -2,6 +2,7 @@ suit = ('Copas','Paus', 'Ouros', 'Espadas')
 rank = ('Dois','Três','Quatro','Cinco','Seis','Sete','Oito','Nove','Dez','Valete', 'Dama', 'Rei','Ás')
 value = {'Dois':2,'Três':3,'Quatro':4,'Cinco':5,'Seis':6,'Sete':7,'Oito':8,'Nove':9,'Dez':10,'Valete':10, 'Dama':10, 'Rei':10,'Ás':11}
 import random
+playing = True
 
 class Hand:
     def __init__(self):
@@ -58,6 +59,45 @@ class Card:
     def __str__(self):
         return self.rank+" de "+self.suit
 
-test_deck = Decks()
-test_deck.shuffle_deck()
-print(test_deck)
+
+def take_bet(total_disp):
+    while True:
+        try:
+            val = int(input("Quanto deseja apostar? "))
+            if val > total_disp:
+                print("Opa, você não tem isso tudo!")
+                continue
+            else:
+                break
+        except:
+            print("Ops! Não é válido. Tem que apostar um número inteiro!")
+
+def hit(Decks, Hand):
+    Hand.pick_card(Decks.deal())
+    Hand.adjust_for_ace_value()
+
+def show_hand(hand):
+    print("Cartas na mão:", *player_hand.cards_in_hand, sep='\n ')
+    print("Valor da mão: ", hand.value)
+
+def hit_stand(Decks, Hand):
+    global playing
+    
+    while playing:
+        choice = input("Digite 'H' para pedir carta e ou 'S' para encerrar seu turno: ")
+        if choice[0].lower() == 'h':
+            hit(Decks,Hand)
+            show_hand(player_hand)
+            print() 
+        elif choice[0].lower() == 's':
+            print("Ok. Agora é o turno da casa.")
+            playing = False
+        else:
+            print('Ops, tente novamente.')
+            continue
+
+
+deck = Decks()
+deck.shuffle_deck()
+player_hand = Hand()
+hit_stand(deck, player_hand)
